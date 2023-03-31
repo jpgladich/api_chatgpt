bbb847c3d9e5080dc053fdddd9c24acd50c0f781
jpgladich <jpgladich@gmail.com>
2023-03-31 15:31:52 -0300

    test

--- api_chatgpt.py
+++ api_chatgpt.py
@@ -14,10 +14,11 @@ def main():
     # Set openai api key
     openai.api_key_path =  "api_key_path/apikey"
     
+    ask = input("Ask: ")
     response = openai.ChatCompletion.create(model = "gpt-3.5-turbo",
-    messages = [{"role": "user", "content": "Grado mayor de la masoneria?"}])
+    messages = [{"role": "user", "content": ask}])
     
-    print(response)
+    print(response.choices[0].message.content)
                                                                                
     
 if __name__ == "__main__":
