2e42fbe05fd0f28c042533a1a3d4bdc3dd947aa2
jpgladich <jpgladich@gmail.com>
2023-03-31 14:11:19 -0300

    Init commit

---
+++ api_chatgpt.py
@@ -0,0 +1,15 @@
+import  openai
+
+
+
+"""
+Info:
+- Module OpenAI: https://github.com/openai/openai-python
+- Doc API ChatGPT: https://platform.openai.com/docs/api-reference/chat
+"""
+
+
+def main():
+    
+    # Set openai api key
+    openai.api_key_path =  "api_key_path/apikey"
