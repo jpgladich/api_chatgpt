0cdd8891835ca16babe7e23d32cb6a175ecab48e
jpgladich <jpgladich@gmail.com>
2023-03-31 17:12:46 -0300

    add input ask while and context

--- api_chatgpt.py
+++ api_chatgpt.py
@@ -1,4 +1,4 @@
-import  openai
+import openai
 
 
 
@@ -13,12 +13,27 @@ def main():
     
     # Set openai api key
     openai.api_key_path =  "api_key_path/apikey"
-    
-    ask = input("Ask: ")
-    response = openai.ChatCompletion.create(model = "gpt-3.5-turbo",
-    messages = [{"role": "user", "content": ask}])
-    
-    print(response.choices[0].message.content)
+        
+    # Set context "system"
+    messages = [{"role": "system", "content": "Assistant Devops, programmer, sysadmin"}]
+        
+    while True:
+
+        ask = input("Ask: ")
+        
+        if ask == "exit":
+            break
+        
+        messages.append({"role": "user", "content": ask})
+        
+        
+        response = openai.ChatCompletion.create(model = "gpt-3.5-turbo",messages = messages)
+        
+        response_ask = response.choices[0].message.content
+        
+        messages.append({"role": "assistant", "content": response_ask})
+        
+        print(response_ask)
                                                                                
     
 if __name__ == "__main__":
