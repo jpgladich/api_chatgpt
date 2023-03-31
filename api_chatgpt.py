21d988565d656686e4def4bc768958c48065a8f7
jpgladich <jpgladich@gmail.com>
2023-03-31 15:14:30 -0300

    test response

--- api_chatgpt.py
+++ api_chatgpt.py
@@ -13,3 +13,12 @@ def main():
     
     # Set openai api key
     openai.api_key_path =  "api_key_path/apikey"
+    
+    response = openai.ChatCompletion.create(model = "gpt-3.5-turbo",
+    messages = [{"role": "user", "content": "Grado mayor de la masoneria?"}])
+    
+    print(response)
+                                                                               
+    
+if __name__ == "__main__":
+    main()
